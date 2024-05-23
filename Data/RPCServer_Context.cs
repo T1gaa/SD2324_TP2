6b84f50fd36734c5c5f582cd506b408d9fa3eb19
JOÃO LUÍS ROCHA DA SILVA <al73618@alunos.utad.pt>
2024-05-24 00:51:28 +0100

    Push existing project to GitLab

---
+++ Data/RPCServer_Context.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace RPCServer.Data
+{
+    public class RPCServer_Context : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Servico> Servicos { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SDTP2324;Trusted_Connection=True;")
+                .LogTo(Console.WriteLine, LogLevel.Information);
+
+        //Utilizamos esta função para "povoar" a tabela dos serviços com alguns serviços
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Servico>().HasData(
+                new Servico
+                {
+                    ServicoId = "SA_T1",
+                    ServicoTipo = "Servico A",
+                    Descricao = "Ronda na zona 1 da cidade",
+                    Estado = "Nao alocado",
+                    ClienteId = null,
+                    AdminName = "admin"
+                },
+                new Servico
+                {
+                    ServicoId = "SA_T2",
+                    ServicoTipo = "Servico A",
+                    Descricao = "Ronda na zona 2 da cidade",
+                    Estado = "Nao alocado",
+                    ClienteId = null,
+                    AdminName = "admin"
+                },
+                new Servico
+                {
+                    ServicoId = "SA_T3",
+                    ServicoTipo = "Servico A",
+                    Descricao = "Ronda na zona 3 da cidade",
+                    Estado = "Nao alocado",
+                    ClienteId = null,
+                    AdminName = "admin"
+                },
+                new Servico
+                {
+                    ServicoId = "SB_T1",
+                    ServicoTipo = "Servico B",
+                    Descricao = "Verificar vegetação na zona dos passadiços do Corgo",
+                    Estado = "Nao alocado",
+                    ClienteId = null,
+                    AdminName = "toze"
+                },
+                new Servico
+                {
+                    ServicoId = "SC_T1",
+                    ServicoTipo = "Servico C",
+                    Descricao = "Verificar árvores da Praça Diogo Cão",
+                    Estado = "Nao alocado",
+                    ClienteId = null,
+                    AdminName = "maria"
+                }
+                );
+
+            modelBuilder.Entity<Admin>().HasData(
+                new Admin
+                {
+                    Username = "admin",
+                    Password = "1234",
+                    Servico = "Servico A"
+                },
+                new Admin
+                {
+                    Username = "toze",
+                    Password = "admin",
+                    Servico = "Servico B"
+                },
+                new Admin
+                {
+                    Username = "maria",
+                    Password = "4321",
+                    Servico = "Servico C"
+                });
+            modelBuilder.Entity<Admin>().HasKey(s => s.Username);
+        }
+    }
+
+    public class Cliente
+    {
+        public string ClienteId { get; set; }
+        public string? Servico { get; set; }
+        public int Idmota { get; set; }
+        public Servico? ServicoId { get; set; }
+    }
+    public class Servico
+    {
+        public string ServicoId { get; set; }
+        public string ServicoTipo { get; set; }
+        public string Descricao { get; set; }
+        public string Estado { get; set; }
+        public string? ClienteId { get; set; }
+        public string? AdminName { get; set; }
+        public Admin? Admin { get; set; }
+        public Cliente? Cliente { get; set; }
+
+    }
+
+    public class Admin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string? Servico { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RPCServer.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+//Inicialização da variável para aceder à base de dados
+await using var db = new RPCServer_Context();
+
+db.Database.EnsureCreated();
+
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+string[] services = {"Servico_A","Servico_B","Servico_C","Servico_D" };
+foreach (var service in services)
+{
+    channel.QueueDeclare(queue: service,
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+}
+
+//channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+var consumer = new EventingBasicConsumer(channel);
+foreach (var service in services)
+{
+    channel.BasicConsume(queue: service,
+                         autoAck: false,
+                         consumer: consumer);
+}
+
+consumer.Received += (model, ea) =>
+{
+    string response = string.Empty;
+
+    var body = ea.Body.ToArray();
+    var props = ea.BasicProperties;
+    var replyProps = channel.CreateBasicProperties();
+    replyProps.CorrelationId = props.CorrelationId;
+
+    try
+        {
+            var message = Encoding.UTF8.GetString(body);
+            string[] parts = message.Split(' ');
+            string username = parts[0];
+            string password = parts[1];
+            Console.WriteLine($" [.] Username({username})");
+            Console.WriteLine($" [.] Password({password})");
+
+            if(Login(username, password) == true)
+            {
+                response = "Autenticado com sucesso";
+            }
+            else 
+            {
+                response = "Credenciais erradas. Ligação ao servidor será encerrada";
+            }
+            bool ok = Regex.IsMatch(username, "Cl_[0-9][0-9][0-9][0-9]");
+            if(ok == false)
+            {
+                message = Encoding.UTF8.GetString(body);
+                if (message == "1")
+                {
+                foreach (var item in ListarServicos(username))
+                {
+                    response = item;
+                }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($" [.] {e.Message}");
+            response = string.Empty;
+        }
+        finally
+        {
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+            channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: props.ReplyTo,
+                                 basicProperties: replyProps,
+                                 body: responseBytes);
+            channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+        }
+};
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+bool Login(string user, string password)
+{
+    
+    if(db.Admins.Any(c => c.Username == user && c.Password == password) == true || db.Clientes.Any(c => c.ClienteId == user) == true)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+    //Se devolver true login com sucesso
+    //Se devolver false login sem sucesso
+}
+
+List<string> ListarServicos(string username)
+{
+    List<string> list = new List<string>();
+    var servicos = db.Servicos.Where(s => s.AdminName == username).ToList();
+    foreach (var servico in servicos)
+    {
+        list.Add(servico.Descricao+ " alocaoda" + servico.ClienteId + "->" + servico.Estado);
+    }
+    return list;
+}
+
+//TODAS AS FUNÇÕES DO SERVIÇO
+//ADMIN: InserirTarefa;ListarServiços;UpdateTarefa;
+//GERAL: LOGIN
+//Garantir que cada cliente (mota e Administrador) esteja sempre associado a um
+//único serviço.
